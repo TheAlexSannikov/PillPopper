ab442b675c614e58eb6eda53d2bacbd3cde02125
TheAlexSannikov <sannikov@wisc.edu>
2022-02-20 00:10:26 -0600

    Basic compose stopwatch from tutorial

--- app/src/main/java/sannikov/a/stonerstopwatch/MainActivity.kt
+++ app/src/main/java/sannikov/a/stonerstopwatch/MainActivity.kt
@@ -1,23 +1,55 @@
 package sannikov.a.stonerstopwatch
 
+import android.graphics.BlendMode
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import sannikov.a.stonerstopwatch.ui.theme.StonerStopwatchTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StonerStopwatchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            Surface(
+                color = Color(0xFF101010),
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Timer(
+                        totalTime = 100L * 1000L,
+                        handleColor = Color.Green,
+                        inactiveBarColor = Color.DarkGray,
+                        activeBarColor = Color(0xFF37B900),
+                        modifier = Modifier.size(200.dp),
+                    )
                 }
             }
         }
@@ -25,14 +57,105 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    StonerStopwatchTheme {
-        Greeting("Android")
+fun Timer(
+    totalTime: Long,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    activeBarColor: Color,
+    modifier: Modifier = Modifier,
+    initArcPercent: Float = 1f,
+    strokeWidth: Dp = 5.dp,
+) {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
     }
+    var arcPercent by remember {
+        mutableStateOf(initArcPercent)
+    }
+    var currentTime by remember {
+        mutableStateOf(totalTime)
+    }
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+    // run code whenever a key changes
+    LaunchedEffect(key1 = currentTime, key2 = isTimerRunning) {
+        if(currentTime > 0 && isTimerRunning) {
+            delay(100L)
+            currentTime -= 100L
+            arcPercent = currentTime / totalTime.toFloat()
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .onSizeChanged {
+                size = it
+            }
+    ) {
+        // draw the circular arc of the stopwatch
+        Canvas(modifier = modifier) {
+            // draw the inactive arc
+            drawArc(
+                color = inactiveBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f,
+                useCenter = false, // else this makes a pie chart
+                topLeft = Offset(0f, 0f), // the default
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round),
+            )
+            // draw the active arc
+            drawArc(
+                color = activeBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f * arcPercent,
+                useCenter = false, // else this makes a pie chart
+                topLeft = Offset(0f, 0f), // the default
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round),
+            )
+            // draw the circle on end of active bar. A little complex
+            val center = Offset(size.width / 2f, size.height / 2f)
+            val angle =
+                (250f * arcPercent + 145f) * (PI / 180f).toFloat() // degree format of the circle
+            val r = size.width / 2f
+            val xOff = cos(angle) * r
+            val yOff = sin(angle) * r
+
+            drawPoints(
+                listOf(Offset(center.x + xOff, center.y + yOff)),
+                pointMode = PointMode.Points,
+                color = handleColor,
+                strokeWidth = (strokeWidth * 3f).toPx(),
+                cap = StrokeCap.Round,
+            )
+        }
+        // the time on clock
+        Text(
+            text = (currentTime / 1000L).toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+        )
+        Button(
+            onClick = { isTimerRunning = !isTimerRunning},
+            modifier = Modifier.align(Alignment.BottomCenter),
+            // can have different colors depending on app state
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = if (!isTimerRunning || currentTime <= 0L) {
+                    Color.Green
+                } else {
+                    Color.Red
+                }
+            )
+        ){
+            Text(text = if(isTimerRunning && currentTime >= 0L) "Stop"
+                else if (!isTimerRunning && currentTime >= 0L) "Start"
+                else "Restart"
+            )
+        }
+    }
+
 }
